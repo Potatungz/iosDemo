f7f9782df3930c926b8c70feef2ecf7f546fbd3b
admin <admin@admins-macbook-air-9.local>
2018-08-20 16:06:07 +0700

    First Commit

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -10,10 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isLightTheme = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.myLabel.text = "Hello World after view load"
     }
+
+    @IBAction func myButton(_ sender: Any) {
+        self.myLabel.text = "Hello \(self.textName.text!)"
+        
+    }
+    @IBAction func changeTheme(_ sender: Any) {
+        view.backgroundColor = UIColor.black
+        
+        for eachView in view.subviews {
+            eachView.backgroundColor = UIColor.black
+            if let label = eachView as? UILabel{
+                label.textColor = UIColor.white
+            }
+            
+            if let textField = eachView as? UITextField{
+                textField.textColor = UIColor.white
+            }
+            
+            if let button = eachView as? UIButton{
+                button.backgroundColor = UIColor.lightGray
+            }
+        }
+        
+    }
+
+    
+    @IBOutlet weak var textName: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
